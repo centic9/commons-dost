7f96f7593eca495525f5e82c33db307c48c02797
Dominik Stadler <dominik.stadler@gmx.at>
2015-05-10 19:40:04 +0200

    Fix Eclipse warning

--- src/main/java/org/dstadler/commons/http/NanoHTTPD.java
+++ src/main/java/org/dstadler/commons/http/NanoHTTPD.java
@@ -1,6 +1,15 @@
 package org.dstadler.commons.http;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -331,11 +340,13 @@ public class NanoHTTPD
 		{
 			try
 			{
-				InputStream is = mySocket.getInputStream();
+				@SuppressWarnings("resource")
+                InputStream is = mySocket.getInputStream();
 				if ( is == null) {
 					return;
 				}
-				BufferedReader in = new BufferedReader( new InputStreamReader( is ));       // NOSONAR - test class works only locally anyway
+				@SuppressWarnings("resource")
+                BufferedReader in = new BufferedReader( new InputStreamReader( is ));       // NOSONAR - test class works only locally anyway
 
 				// Read the request line
 				String inLine = in.readLine();
