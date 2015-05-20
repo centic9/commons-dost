520b5c62aaa27951b3cf87f2971bcc05a66488af
Dominik Stadler <dominik.stadler@gmx.at>
2015-05-20 16:57:56 +0200

    Add utility class for running dot from Grapviz

---
+++ src/main/java/org/dstadler/commons/graphviz/DotUtils.java
@@ -0,0 +1,72 @@
+package org.dstadler.commons.graphviz;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.exec.ExecuteWatchdog;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.apache.commons.lang3.SystemUtils;
+
+/**
+ * Simple utility methods to run the dot-tool from Graphviz on a file.
+ *
+ * @author dominik.stadler
+ *
+ */
+public class DotUtils {
+	public static final String DOT_EXE = SystemUtils.IS_OS_WINDOWS ? "C:\\cygwin\\bin\\dot.exe" : "/usr/bin/dot";
+	private static final String PNG_TYPE = "png";
+
+	/**
+	 * Call graphviz-dot to convert the .dot-file to a PNG
+	 *
+	 * @param dotfile
+	 * @return resulting file or null if it failed.
+	 *
+	 * @throws IOException
+	 */
+	public static File renderGraph(File dotfile) throws IOException {
+		File out = new File(dotfile.getAbsolutePath() + "." + PNG_TYPE);
+
+		// call graphviz-dot via commons-exec
+		CommandLine cmdLine = new CommandLine(DOT_EXE);
+		cmdLine.addArgument("-T" + PNG_TYPE);
+		cmdLine.addArgument(dotfile.getAbsolutePath());
+		DefaultExecutor executor = new DefaultExecutor();
+		executor.setExitValue(0);
+		ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
+		executor.setWatchdog(watchdog);
+		try (FileOutputStream out2 = new FileOutputStream(out)) {
+			executor.setStreamHandler(new PumpStreamHandler(out2, System.err));
+			executor.execute(cmdLine);
+		}
+
+		return out;
+	}
+
+	/**
+	 * Verify if dot can be started and print out the version to stdout.
+	 *
+	 * @return True if "dot -V" ran successfully, false otherwise
+	 *
+	 * @throws ExecuteException
+	 * @throws IOException
+	 */
+	public static boolean checkDot() throws IOException {
+		// call graphviz-dot via commons-exec
+		CommandLine cmdLine = new CommandLine(DOT_EXE);
+		cmdLine.addArgument("-V");
+		DefaultExecutor executor = new DefaultExecutor();
+		executor.setExitValue(0);
+		ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
+		executor.setWatchdog(watchdog);
+		executor.setStreamHandler(new PumpStreamHandler(System.out, System.err));
+		executor.execute(cmdLine);
+
+		return true;
+	}
+}
---
+++ src/test/java/org/dstadler/commons/graphviz/DotUtilsTest.java
@@ -0,0 +1,65 @@
+package org.dstadler.commons.graphviz;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DotUtilsTest {
+	private static final String DOT_FILE = "digraph G {" +
+		"dpi=300;\n" +
+		"rankdir=LR;\n" +
+		"node [shape=box];\n" +
+		"\n" +
+		"\"agent15\"->\"c.d.d.agent\";\n" +
+		"\"com.dynatrace.adk\"->\"c.d.d.agent\";\n" +
+		"\"c.d.d.adm\"->\"test.util.nonprod\";\n" +
+		"\"c.d.d.adm\"->\"c.d.d.sdk\";\n" +
+		"\"c.d.d.adm\"->\"c.d.d.portlets\";\n" +
+		"\"c.d.d.adm\"->\"c.d.d.server.shared\";\n" +
+		"\"c.d.d.adm\"->\"c.d.d.util\";\n" +
+		"}\n";
+
+	@Before
+	public void setUp() throws IOException {
+		boolean exists = new File(DotUtils.DOT_EXE).exists();
+
+		assertEquals(exists, DotUtils.checkDot());
+		Assume.assumeTrue("Did not find dot executable at " + DotUtils.DOT_EXE, exists);
+	}
+
+	@Test
+	public void testRenderGraph() throws Exception {
+		File file = File.createTempFile("DotUtilsTest", ".dot");
+		try {
+			FileUtils.write(file, DOT_FILE);
+
+			File retfile = DotUtils.renderGraph(file);
+			assertNotNull(retfile);
+		} finally {
+			assertTrue(file.delete());
+		}
+	}
+
+	@Test
+	public void testRenderGraphInvalidFile() throws Exception {
+		File file = File.createTempFile("DotUtilsTest", ".dot");
+		try {
+			FileUtils.write(file, "{ digraph");
+
+			try {
+				DotUtils.renderGraph(file);
+				fail("Should catch exception");
+			} catch (IOException e) {
+				// expected here
+			}
+		} finally {
+			assertTrue(file.delete());
+		}
+	}
+}
