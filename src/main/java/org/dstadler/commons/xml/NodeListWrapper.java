78c656605df9acd3f1a127300fc64d11c13490b5
Dominik Stadler <dominik.stadler@gmx.at>
2015-09-16 15:42:31 +0200

    Add new class NodeListWrapper to have a List-based view on a DOM-NodeList

---
+++ src/main/java/org/dstadler/commons/xml/NodeListWrapper.java
@@ -0,0 +1,169 @@
+package org.dstadler.commons.xml;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * Simple wrapper to allow to iterate over a NodeList.
+ *
+ * Note: some methods are not implemented yet, only the most basic ones are done currently.
+ * Also this is a read-only List, so any method that would change the List is not supported as well.
+ *
+ * @author dominik.stadler
+ *
+ */
+public class NodeListWrapper implements List<Node> {
+	private final class LocalIterator implements Iterator<Node> {
+		int nextPos = 0;
+
+		@Override
+		public boolean hasNext() {
+			return nodeList.getLength() > nextPos;
+		}
+
+		@Override
+		public Node next() {
+			if(nextPos >= nodeList.getLength()) {
+				throw new NoSuchElementException("Cannot access beyound end of iterator, had " + nodeList.getLength() + " items, tried to access item at position " + nextPos + ".");
+			}
+
+			Node node = nodeList.item(nextPos);
+			nextPos++;
+			return node;
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	private final NodeList nodeList;
+
+	public NodeListWrapper(NodeList nodeList) {
+		super();
+		this.nodeList = nodeList;
+	}
+
+	@Override
+	public int size() {
+		return nodeList.getLength();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return nodeList.getLength() == 0;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterator<Node> iterator() {
+		return new LocalIterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean add(Node e) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends Node> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends Node> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Node get(int index) {
+		return nodeList.item(index);
+	}
+
+	@Override
+	public Node set(int index, Node element) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void add(int index, Node element) {
+		throw new UnsupportedOperationException();
+
+	}
+
+	@Override
+	public Node remove(int index) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<Node> listIterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<Node> listIterator(int index) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<Node> subList(int fromIndex, int toIndex) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/test/java/org/dstadler/commons/xml/NodeListWrapperTest.java
@@ -0,0 +1,226 @@
+package org.dstadler.commons.xml;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import org.dstadler.commons.testing.TestHelpers;
+
+public class NodeListWrapperTest {
+	@Test
+	public void testNodeListWrapper() {
+		NodeListWrapper wrapper = new NodeListWrapper(new EmptyNodeList());
+		assertEquals(0, wrapper.size());
+		assertTrue(wrapper.isEmpty());
+
+		wrapper = new NodeListWrapper(new CountNodeList(23));
+		assertEquals(23, wrapper.size());
+		assertFalse(wrapper.isEmpty());
+		for(Node node : wrapper) {
+			assertNull(node);
+		}
+
+		assertNull(wrapper.get(2));
+	}
+
+	@Test
+	public void testNodeListWrapperReadOverTheEnd() {
+		NodeListWrapper wrapper = new NodeListWrapper(new CountNodeList(23));
+		assertEquals(23, wrapper.size());
+		assertFalse(wrapper.isEmpty());
+
+		Iterator<Node> it = wrapper.iterator();
+
+		while(it.hasNext()) {
+			assertNull(it.next());
+		}
+
+		// ensure that we get an error if we access out of bounds
+		try {
+			it.next();
+		} catch (NoSuchElementException e) {
+			TestHelpers.assertContains(e, "Cannot access beyound end of iterator", "23");
+		}
+	}
+
+	@Test
+	public void testNodeListWrapperUnsupportedOperations() {
+		NodeListWrapper wrapper = new NodeListWrapper(new CountNodeList(12));
+
+		try {
+			wrapper.iterator().remove();
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.contains(null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.toArray();
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.toArray(new String[] {});
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.add(null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.remove(null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.addAll(null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.containsAll(null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.addAll(2, null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.removeAll(null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.retainAll(null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.clear();
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.set(1, null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.add(3, null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.remove(2);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.indexOf(null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.lastIndexOf(null);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.listIterator();
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.listIterator(4);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+
+		try {
+			wrapper.subList(2, 6);
+			fail("should throw exception");
+		} catch (UnsupportedOperationException e) {
+			assertNull(e.getMessage());
+		}
+	}
+
+	private static final class CountNodeList implements NodeList {
+		int count;
+
+		public CountNodeList(int count) {
+			super();
+			this.count = count;
+		}
+
+		@Override
+		public Node item(int index) {
+			return null;
+		}
+
+		@Override
+		public int getLength() {
+			return count;
+		}
+	}
+
+	private static final class EmptyNodeList implements NodeList {
+		@Override
+		public Node item(int index) {
+			throw new IllegalStateException();
+		}
+
+		@Override
+		public int getLength() {
+			return 0;
+		}
+	}
+}
