306b40933703a00815eb0d8cf855b9b9950b320d
Dominik Stadler <dominik.stadler@gmx.at>
2016-05-12 08:53:15 +0200

    Add method to MapUtils to sort by value first and key for equal values, the existing sortByValue() has undefined sort-order in this case.

--- src/main/java/org/dstadler/commons/collections/MapUtils.java
+++ src/main/java/org/dstadler/commons/collections/MapUtils.java
@@ -41,6 +41,28 @@ public final class MapUtils {
         return entries;
     }
 
+    /**
+     * Sorts the provided Map by the value and then by key instead of by key like TreeMap does.
+     *
+     * The Value-Type and Key-Type need to implement the Comparable-interface.
+     *
+     * Note: this will require one Map.Entry for each element in the Map, so for very large
+     * Maps this will incur some memory overhead but will not fully duplicate the contents of the map.
+     *
+     * @param <K> the key-type of the Map that should be sorted Needs to derive from {@link Comparable}
+     * @param <V> the value-type of the Map that should be sorted. Needs to derive from {@link Comparable}
+     *
+     * @param map A map with some elements which should be sorted by Value first and then by Key.
+     *
+     *
+     * @return Returns a new List of Map.Entry values sorted by value and key.
+     */
+    public static <K extends Comparable<K>, V extends Comparable<V>> List<Map.Entry<K, V>> sortByValueAndKey(Map<K, V> map) {
+        List<Map.Entry<K, V>> entries = new ArrayList<>(map.entrySet());
+        Collections.sort(entries, new ByValueAndKey<K,V>());
+        return entries;
+    }
+
     // Helper class for sorting maps by value
     private static class ByValue<K, V extends Comparable<V>> implements Comparator<Entry<K, V>>, Serializable {
         private static final long serialVersionUID = 1L;
@@ -48,6 +70,22 @@ public final class MapUtils {
         @Override
         public int compare(Entry<K, V> o1, Entry<K, V> o2) {
             return o1.getValue().compareTo(o2.getValue());
+        }
+    }
+
+    // Helper class for sorting maps by value and key
+    private static class ByValueAndKey<K extends Comparable<K>, V extends Comparable<V>> implements Comparator<Map.Entry<K, V>>, Serializable {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+            int ret = o1.getValue().compareTo(o2.getValue());
+            if(ret != 0) {
+                return ret;
+            }
+
+            // on equal value, sort by key
+            return o1.getKey().compareTo(o2.getKey());
         }
     }
 }
--- src/test/java/org/dstadler/commons/collections/MapUtilsTest.java
+++ src/test/java/org/dstadler/commons/collections/MapUtilsTest.java
@@ -17,7 +17,7 @@ import org.dstadler.commons.testing.PrivateConstructorCoverage;
 public class MapUtilsTest {
 
 	@Test
-	public void testSorting() {
+	public void testSortingByValue() {
 		HashMap<String, Integer> map = new HashMap<>();
 		map.put("One", 1);
 		map.put("Two", 2);
@@ -27,6 +27,35 @@ public class MapUtilsTest {
 		assertEquals("First", "One", sorted.get(0).getKey());
 		assertEquals("Second", "Two", sorted.get(1).getKey());
 		assertEquals("Third", "Three", sorted.get(2).getKey());
+	}
+
+	@Test
+	public void testSortingByValueAndKey() {
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("One", 1);
+		map.put("Two", 2);
+		map.put("Three", 3);
+
+		List<Map.Entry<String, Integer>> sorted = MapUtils.sortByValueAndKey(map);
+		assertEquals("First", "One", sorted.get(0).getKey());
+		assertEquals("Second", "Two", sorted.get(1).getKey());
+		assertEquals("Third", "Three", sorted.get(2).getKey());
+
+		map.put("Threa", 3);
+		map.put("Threb", 3);
+		map.put("Threc", 3);
+		map.put("Thred", 3);
+		map.put("Thref", 3);
+
+		sorted = MapUtils.sortByValueAndKey(map);
+		assertEquals("First", "One", sorted.get(0).getKey());
+		assertEquals("Second", "Two", sorted.get(1).getKey());
+		assertEquals("Third", "Threa", sorted.get(2).getKey());
+		assertEquals("Third", "Threb", sorted.get(3).getKey());
+		assertEquals("Third", "Threc", sorted.get(4).getKey());
+		assertEquals("Third", "Thred", sorted.get(5).getKey());
+		assertEquals("Third", "Three", sorted.get(6).getKey());
+		assertEquals("Third", "Thref", sorted.get(7).getKey());
 	}
 
 	// helper method to get coverage of the unused constructor
