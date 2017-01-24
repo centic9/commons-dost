bb15adf7f37b9af90c11cd6c526ccea926683fee
Dominik Stadler <dominik.stadler@gmx.at>
2017-01-24 15:04:50 +0100

    Handle null-keys in MappedCounter without throwing a NullPointerException

--- src/main/java/org/dstadler/commons/collections/MappedCounterImpl.java
+++ src/main/java/org/dstadler/commons/collections/MappedCounterImpl.java
@@ -13,80 +13,92 @@ import java.util.*;
  */
 public class MappedCounterImpl<T> implements MappedCounter<T> {
 
-	// sort on values, highest value first
-	private Map<T, Integer> map = new HashMap<>();
+    // sort on values, highest value first
+    private Map<T, Integer> map = new HashMap<>();
 
-	@Override
-	public void addInt(T k, int v) {
-		if (!map.containsKey(k)) {
-			map.put(k, v);
-		} else {
-			map.put(k, map.get(k) + v);
-		}
-	}
+    @Override
+    public void addInt(T k, int v) {
+        if (!map.containsKey(k)) {
+            map.put(k, v);
+        } else {
+            map.put(k, map.get(k) + v);
+        }
+    }
 
-	@Override
-	public void count(Collection<T> items) {
-		for(T item : items) {
-			addInt(item, 1);
-		}
-	}
+    @Override
+    public void count(Collection<T> items) {
+        for(T item : items) {
+            addInt(item, 1);
+        }
+    }
 
-	@Override
-	public int get(T k) {
-		return map.containsKey(k) ? map.get(k) : 0;
-	}
+    @Override
+    public int get(T k) {
+        return map.containsKey(k) ? map.get(k) : 0;
+    }
 
-	@Override
-	public int remove(T key) {
-		return map.remove(key);
-	}
+    @Override
+    public int remove(T key) {
+        return map.remove(key);
+    }
 
-	@Override
-	public Set<T> keys() {
-		return map.keySet();
-	}
+    @Override
+    public Set<T> keys() {
+        return map.keySet();
+    }
 
-	@Override
-	public Set<Map.Entry<T, Integer>> entries() {
-		return map.entrySet();
-	}
+    @Override
+    public Set<Map.Entry<T, Integer>> entries() {
+        return map.entrySet();
+    }
 
-	@Override
-	public Map<T, Integer> sortedMap() {
-		List<Map.Entry<T, Integer>> list = new LinkedList<>(map.entrySet());
-		Collections.sort(list, new Comparator<Map.Entry<T, Integer>>() {
+    @Override
+    public Map<T, Integer> sortedMap() {
+        List<Map.Entry<T, Integer>> list = new LinkedList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<T, Integer>>() {
 
-			@Override
-			public int compare(Map.Entry<T, Integer> o1, Map.Entry<T, Integer> o2) {
-				// reverse ordering to get highest values first
-				int ret = (-1) * o1.getValue().compareTo(o2.getValue());
-				if(ret != 0) {
-					return ret;
-				}
+            @Override
+            public int compare(Map.Entry<T, Integer> o1, Map.Entry<T, Integer> o2) {
+                // reverse ordering to get highest values first
+                int ret = (-1) * o1.getValue().compareTo(o2.getValue());
+                if(ret != 0) {
+                    return ret;
+                }
 
-				return o1.getKey().toString().compareTo(o2.getKey().toString());
-			}
-		});
+                final T key1 = o1.getKey();
+                final T key2 = o2.getKey();
 
-		Map<T, Integer> result = new LinkedHashMap<>();
-		for (Map.Entry<T, Integer> entry : list) {
-			result.put(entry.getKey(), entry.getValue());
-		}
-		return result;
-	}
+                // we use a HashMap which allows null-keys
+                if(key1 == null && key2 == null) {
+                    return 0;
+                } else if(key1 == null) {
+                    return -1;
+                } else if(key2 == null) {
+                    return 1;
+                }
 
-	@Override
-	public int sum() {
-		int sum = 0;
-		for(Integer i : map.values()) {
-			sum += i;
-		}
-		return sum;
-	}
+                return key1.toString().compareTo(key2.toString());
+            }
+        });
 
-	@Override
-	public String toString() {
-		return map.toString();
-	}
+        Map<T, Integer> result = new LinkedHashMap<>();
+        for (Map.Entry<T, Integer> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+
+    @Override
+    public int sum() {
+        int sum = 0;
+        for(Integer i : map.values()) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        return map.toString();
+    }
 }
--- src/test/java/org/dstadler/commons/collections/MappedCounterTest.java
+++ src/test/java/org/dstadler/commons/collections/MappedCounterTest.java
@@ -12,146 +12,172 @@ import java.util.Map.Entry;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-
-
 public class MappedCounterTest {
-	@Test
-	public void test() {
-		MappedCounter<String> counter = new MappedCounterImpl<>();
-		assertEquals(0, counter.get("some"));
-		assertNotNull(counter.keys());
-		assertNotNull(counter.entries());
+    @Test
+    public void test() {
+        MappedCounter<String> counter = new MappedCounterImpl<>();
+        assertEquals(0, counter.get("some"));
+        assertNotNull(counter.keys());
+        assertNotNull(counter.entries());
 
-		counter.addInt("test", 1);
-		assertEquals(1, counter.get("test"));
+        counter.addInt("test", 1);
+        assertEquals(1, counter.get("test"));
 
-		counter.addInt("test", 2);
-		assertEquals(3, counter.get("test"));
+        counter.addInt("test", 2);
+        assertEquals(3, counter.get("test"));
 
-		counter.addInt("other", 14);
-		counter.addInt("third", 5);
-		assertEquals(3, counter.get("test"));
-		assertEquals(14, counter.get("other"));
-		assertEquals(5, counter.get("third"));
+        counter.addInt("other", 14);
+        counter.addInt("third", 5);
+        assertEquals(3, counter.get("test"));
+        assertEquals(14, counter.get("other"));
+        assertEquals(5, counter.get("third"));
 
-		Map<String, Integer> sortedMap = counter.sortedMap();
-		Iterator<Entry<String, Integer>> iterator = sortedMap.entrySet().iterator();
+        Map<String, Integer> sortedMap = counter.sortedMap();
+        Iterator<Entry<String, Integer>> iterator = sortedMap.entrySet().iterator();
 
-		// sorted by decreasing value
-		assertEquals(Integer.valueOf(14), iterator.next().getValue());
-		assertEquals(Integer.valueOf(5), iterator.next().getValue());
-		assertEquals(Integer.valueOf(3), iterator.next().getValue());
+        // sorted by decreasing value
+        assertEquals(Integer.valueOf(14), iterator.next().getValue());
+        assertEquals(Integer.valueOf(5), iterator.next().getValue());
+        assertEquals(Integer.valueOf(3), iterator.next().getValue());
 
-		assertEquals(22, counter.sum());
-	}
+        assertEquals(22, counter.sum());
+    }
 
-	@Test
-	public void testSortedMapSorted() {
-		MappedCounter<String> counter = new MappedCounterImpl<>();
+    @Test
+    public void testSortedMapSorted() {
+        MappedCounter<String> counter = new MappedCounterImpl<>();
 
-		counter.addInt("test", 1);
-		assertEquals(1, counter.get("test"));
-		counter.addInt("other", 1);
-		counter.addInt("third", 1);
-		counter.addInt("fourth", 1);
-		counter.addInt("fifth", 1);
-		assertEquals(1, counter.get("test"));
-		assertEquals(1, counter.get("other"));
-		assertEquals(1, counter.get("third"));
-		assertEquals(1, counter.get("fourth"));
-		assertEquals(1, counter.get("fifth"));
+        counter.addInt("test", 1);
+        assertEquals(1, counter.get("test"));
+        counter.addInt("other", 1);
+        counter.addInt("third", 1);
+        counter.addInt("fourth", 1);
+        counter.addInt("fifth", 1);
+        assertEquals(1, counter.get("test"));
+        assertEquals(1, counter.get("other"));
+        assertEquals(1, counter.get("third"));
+        assertEquals(1, counter.get("fourth"));
+        assertEquals(1, counter.get("fifth"));
 
-		assertEquals("{fifth=1, fourth=1, other=1, test=1, third=1}", counter.sortedMap().toString());
-		assertEquals(5, counter.sum());
-	}
+        assertEquals("{fifth=1, fourth=1, other=1, test=1, third=1}", counter.sortedMap().toString());
+        assertEquals(5, counter.sum());
+    }
 
-	@Test
-	public void testGeneric() {
-		MappedCounter<Integer> counter = new MappedCounterImpl<>();
-		assertEquals(0, counter.get(24));
-		assertNotNull(counter.keys());
-		assertNotNull(counter.entries());
+    @Test
+    public void testGeneric() {
+        MappedCounter<Integer> counter = new MappedCounterImpl<>();
+        assertEquals(0, counter.get(24));
+        assertNotNull(counter.keys());
+        assertNotNull(counter.entries());
 
-		counter.addInt(26, 1);
-		assertEquals(1, counter.get(26));
+        counter.addInt(26, 1);
+        assertEquals(1, counter.get(26));
 
-		counter.addInt(26, 2);
-		assertEquals(3, counter.get(26));
+        counter.addInt(26, 2);
+        assertEquals(3, counter.get(26));
 
-		counter.addInt(88, 14);
-		counter.addInt(-3432, 5);
-		assertEquals(3, counter.get(26));
-		assertEquals(14, counter.get(88));
-		assertEquals(5, counter.get(-3432));
+        counter.addInt(88, 14);
+        counter.addInt(-3432, 5);
+        assertEquals(3, counter.get(26));
+        assertEquals(14, counter.get(88));
+        assertEquals(5, counter.get(-3432));
 
-		Map<Integer, Integer> sortedMap = counter.sortedMap();
-		Iterator<Entry<Integer, Integer>> iterator = sortedMap.entrySet().iterator();
+        Map<Integer, Integer> sortedMap = counter.sortedMap();
+        Iterator<Entry<Integer, Integer>> iterator = sortedMap.entrySet().iterator();
 
-		// sorted by decreasing value
-		assertEquals(Integer.valueOf(14), iterator.next().getValue());
-		assertEquals(Integer.valueOf(5), iterator.next().getValue());
-		assertEquals(Integer.valueOf(3), iterator.next().getValue());
-	}
+        // sorted by decreasing value
+        assertEquals(Integer.valueOf(14), iterator.next().getValue());
+        assertEquals(Integer.valueOf(5), iterator.next().getValue());
+        assertEquals(Integer.valueOf(3), iterator.next().getValue());
+    }
 
-	@Test
-	public void testToString() {
-		MappedCounter<String> counter = new MappedCounterImpl<>();
-		TestHelpers.ToStringTest(counter);
+    @Test
+    public void testToString() {
+        MappedCounter<String> counter = new MappedCounterImpl<>();
+        TestHelpers.ToStringTest(counter);
 
-		counter.addInt("Str1", 3);
-		counter.addInt("Str4", 6);
+        counter.addInt("Str1", 3);
+        counter.addInt("Str4", 6);
 
-		TestHelpers.ToStringTest(counter);
+        TestHelpers.ToStringTest(counter);
 
-		TestHelpers.assertContains(counter.toString(), "Str1", "Str4", "6");
-	}
+        TestHelpers.assertContains(counter.toString(), "Str1", "Str4", "6");
+    }
 
-	@Test
-	public void testToStringGeneric() {
-		MappedCounter<Integer> counter = new MappedCounterImpl<>();
-		TestHelpers.ToStringTest(counter);
+    @Test
+    public void testToStringGeneric() {
+        MappedCounter<Integer> counter = new MappedCounterImpl<>();
+        TestHelpers.ToStringTest(counter);
 
-		counter.addInt(234, 3);
-		counter.addInt(754, 6);
+        counter.addInt(234, 3);
+        counter.addInt(754, 6);
 
-		TestHelpers.ToStringTest(counter);
+        TestHelpers.ToStringTest(counter);
 
-		TestHelpers.assertContains(counter.toString(), "234", "754", "6");
-	}
+        TestHelpers.assertContains(counter.toString(), "234", "754", "6");
+    }
 
-	@Test
-	public void testRemove() {
-		MappedCounter<String> counter = new MappedCounterImpl<>();
-		assertEquals(0, counter.get("some"));
-		assertNotNull(counter.keys());
-		assertNotNull(counter.entries());
+    @Test
+    public void testRemove() {
+        MappedCounter<String> counter = new MappedCounterImpl<>();
+        assertEquals(0, counter.get("some"));
+        assertNotNull(counter.keys());
+        assertNotNull(counter.entries());
 
-		counter.addInt("test", 1);
-		assertEquals(1, counter.get("test"));
+        counter.addInt("test", 1);
+        assertEquals(1, counter.get("test"));
 
-		counter.remove("test");
-		assertEquals(0, counter.get("test"));
-	}
+        counter.remove("test");
+        assertEquals(0, counter.get("test"));
+    }
 
-	@Test
-	public void testCount() {
-		MappedCounter<String> counter = new MappedCounterImpl<>();
-		assertEquals(0, counter.get("some"));
+    @Test
+    public void testCount() {
+        MappedCounter<String> counter = new MappedCounterImpl<>();
+        assertEquals(0, counter.get("some"));
 
-		counter.count(Collections.<String>emptyList());
-		assertEquals(0, counter.sortedMap().size());
+        counter.count(Collections.<String>emptyList());
+        assertEquals(0, counter.sortedMap().size());
 
-		counter.count(Collections.singleton("some"));
-		assertEquals(1, counter.sortedMap().size());
-		assertEquals(1, counter.get("some"));
+        counter.count(Collections.singleton("some"));
+        assertEquals(1, counter.sortedMap().size());
+        assertEquals(1, counter.get("some"));
 
-		counter.count(Collections.singleton("some"));
-		assertEquals(1, counter.sortedMap().size());
-		assertEquals(2, counter.get("some"));
+        counter.count(Collections.singleton("some"));
+        assertEquals(1, counter.sortedMap().size());
+        assertEquals(2, counter.get("some"));
 
-		counter.count(Arrays.asList("some", "some2", "some2"));
-		assertEquals(2, counter.sortedMap().size());
-		assertEquals(3, counter.get("some"));
-		assertEquals(2, counter.get("some2"));
-	}
+        counter.count(Arrays.asList("some", "some2", "some2"));
+        assertEquals(2, counter.sortedMap().size());
+        assertEquals(3, counter.get("some"));
+        assertEquals(2, counter.get("some2"));
+    }
+
+    @Test
+    public void testNullKey() {
+        MappedCounter<String> counter = new MappedCounterImpl<>();
+
+        counter.addInt(null, 1);
+
+        assertEquals(1, counter.get(null));
+        assertEquals("{null=1}", counter.sortedMap().toString());
+
+        counter.addInt("some", 2);
+        counter.addInt("more", 3);
+
+        assertEquals(1, counter.get(null));
+        assertEquals("{more=3, some=2, null=1}", counter.sortedMap().toString());
+
+        // NPE with the same value
+        counter.addInt("other", 1);
+
+        assertEquals(1, counter.get(null));
+        assertEquals("{more=3, some=2, null=1, other=1}", counter.sortedMap().toString());
+
+        // NPE with the same value
+        counter.addInt("third", 1);
+
+        assertEquals(1, counter.get(null));
+        assertEquals("{more=3, some=2, null=1, other=1, third=1}", counter.sortedMap().toString());
+    }
 }
