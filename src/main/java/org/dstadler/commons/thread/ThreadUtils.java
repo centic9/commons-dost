075119a7733da8795873e5350b953780e528aef9
Dominik Stadler <dominik.stadler@gmx.at>
2021-01-30 13:35:12 +0100

    Add ThreadUtils to lookup running threads and stopping a thread

---
+++ src/main/java/org/dstadler/commons/thread/ThreadUtils.java
@@ -0,0 +1,90 @@
+package org.dstadler.commons.thread;
+
+import org.dstadler.commons.logging.jdk.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ThreadUtils {
+    private final static Logger log = LoggerFactory.make();
+
+    /**
+     * Returns a list of all threads with the given name, i.e. equals-match.
+     *
+     * @param threadName The name to look for.
+     * @return A list of found threads. Returns an empty list if no thread is found.
+     */
+    public static List<Thread> getThreadsByName(String threadName) {
+        int count = Thread.currentThread().getThreadGroup().activeCount();
+
+        Thread[] threads = new Thread[count];
+        Thread.currentThread().getThreadGroup().enumerate(threads);
+
+        List<Thread> foundThreads = new ArrayList<>();
+        for (Thread t : threads) {
+            if (t != null && t.getName().equals(threadName)) {
+                foundThreads.add(t);
+            }
+        }
+        return foundThreads;
+    }
+
+    /**
+     * Returns the first thread for which the thread-name contains
+     * the given string.
+     *
+     * The order of searching is undefined, any of the matching
+     * threads might be returned.
+     *
+     * @param contains The string to look for.
+     * @return The first matching thread or null if no thread is found.
+     */
+    public static Thread lookupThread(String contains) {
+        int count = Thread.currentThread().getThreadGroup().activeCount();
+
+        Thread[] threads = new Thread[count];
+        Thread.currentThread().getThreadGroup().enumerate(threads);
+
+        for (Thread t : threads) {
+            if (t != null && t.getName().contains(contains)) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Try to wait for the thread to stop gracefully. If it does not
+     * stop by itself, first tries to interrupt the thread and after
+     * waiting some more, will finally forcefully stop the thread.
+     *
+     * @param thread The thread to stop
+     * @param joinTimeMs How long to wait for the thread to stop
+     * @throws InterruptedException If the currently executing thread
+     *          was interrupted while waiting
+     */
+    public static void stopThread(Thread thread, int joinTimeMs) throws InterruptedException {
+        // let thread shut down on it's own first
+        thread.join(1000);
+
+        // then ensure any blocked operation is interrupted
+        thread.interrupt();
+
+        // wait a bit for the thread to actually shut down
+        try {
+            thread.join(joinTimeMs);
+        } catch (InterruptedException e) {
+            log.log(Level.WARNING, "Joining thread was interrupted", e);
+        }
+
+        // if not stopped yet, forcefully stop it
+        try {
+            //noinspection deprecation
+            thread.stop();
+        } catch (ThreadDeath e) {
+            log.log(Level.WARNING, "Stopping thread threw an exception", e);
+        }
+    }
+}
---
+++ src/test/java/org/dstadler/commons/thread/ThreadUtilsTest.java
@@ -0,0 +1,117 @@
+package org.dstadler.commons.thread;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.dstadler.commons.testing.ThreadTestHelper;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class ThreadUtilsTest {
+    private final AtomicReference<Throwable> exc = new AtomicReference<>();
+
+    @After
+    public void tearDown() {
+        if (exc.get() != null) {
+            fail("Did not expect an exception but had: " + ExceptionUtils.getStackTrace(exc.get()));
+        }
+
+        ThreadTestHelper.assertNoThreadLeft("Had a test-thread still running at the end of testing",
+                "TestUtilsTest");
+    }
+
+    @Test
+    public void testGetThreadsByNameNotFound() {
+        final List<Thread> threads = ThreadUtils.getThreadsByName("not existing");
+        assertEquals("Had: " + threads,
+                0, threads.size());
+    }
+
+    @Test
+    public void testGetThreadsByNameOne() {
+        final List<Thread> threads = ThreadUtils.getThreadsByName(Thread.currentThread().getName());
+        assertEquals("Had: " + threads,
+                1, threads.size());
+    }
+
+    @Test
+    public void testGetThreadsByNameMultiple() throws InterruptedException {
+        CountDownLatch startLatch = new CountDownLatch(3);
+        CountDownLatch latch = new CountDownLatch(1);
+
+        final Thread[] startedThreads = new Thread[3];
+        for(int i = 0;i < 3;i++) {
+            startedThreads[i] = new Thread(() -> {
+                startLatch.countDown();
+                try {
+                    latch.await();
+                } catch (Throwable e) {
+                    exc.set(e);
+                }
+            }, "TestUtilsTest-thread");
+
+            startedThreads[i].start();
+        }
+
+        startLatch.await();
+
+        final List<Thread> threads = ThreadUtils.getThreadsByName("TestUtilsTest-thread");
+        assertEquals("Had: " + threads,
+                3, threads.size());
+
+        // allow threads to finish
+        latch.countDown();
+
+        for(int i = 0;i < 3;i++) {
+            startedThreads[i].join();
+        }
+
+    }
+
+    @Test
+    public void testStopStartedThread() throws InterruptedException {
+        CountDownLatch startLatch = new CountDownLatch(1);
+
+        assertNull("No such thread running before",
+                ThreadUtils.lookupThread("TestUtilsTest"));
+
+        Thread testThread = new Thread(() -> {
+            startLatch.countDown();
+
+            try {
+                Thread.sleep(60_000);
+            } catch (InterruptedException e) {
+                // this exception is expected here
+            } catch (Throwable e) {
+                exc.set(e);
+            }
+        }, "TestUtilsTest");
+
+        testThread.start();
+
+        startLatch.await();
+
+        Thread thread = ThreadUtils.lookupThread("TestUtilsTest");
+        assertNotNull("There is a thread running now",
+                thread);
+        assertTrue(thread.isAlive());
+
+        ThreadUtils.stopThread(testThread, 1000);
+
+        thread.join();
+
+        assertFalse(thread.isAlive());
+
+        assertNull("No such thread running afterwards",
+                ThreadUtils.lookupThread("TestUtilsTest"));
+    }
+}
